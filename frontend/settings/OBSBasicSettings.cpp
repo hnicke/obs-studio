ebe2407f04ca3f10d19ec4ecba03bddea40c9047
Heiko Nickerl <mail@hnicke.de>
2026-03-29 18:11:54 +0200

    feat(ui): remove multitrack video

--- frontend/settings/OBSBasicSettings.cpp
+++ frontend/settings/OBSBasicSettings.cpp
@@ -1,17 +1,17 @@
 /******************************************************************************
  Copyright (C) 2023 by Lain Bailey <lain@obsproject.com>
  Philippe Groarke <philippe.groarke@gmail.com>
- 
+
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 2 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -5614,7 +5614,8 @@ void OBSBasicSettings::UpdateMultitrackVideo()
 			ui->enableMultitrackVideo->setChecked(false);
 	}
 
-	ui->multitrackVideoGroupBox->setVisible(available);
+	// Keep multitrack controls hidden in the Stream settings UI.
+	ui->multitrackVideoGroupBox->setVisible(false);
 
 	ui->enableMultitrackVideo->setEnabled(toggle_available);
 
