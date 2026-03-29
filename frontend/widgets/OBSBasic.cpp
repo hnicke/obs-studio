fe3d0186e4c75ab589859d54f6684fdeffa05c0a
Heiko Nickerl <mail@hnicke.de>
2026-03-29 19:33:00 +0200

    feat(ui): disable profiles menu

--- frontend/widgets/OBSBasic.cpp
+++ frontend/widgets/OBSBasic.cpp
@@ -254,6 +254,7 @@ OBSBasic::OBSBasic(QWidget *parent) : OBSMainWindow(parent), undo_s(ui), ui(new 
 	api = InitializeAPIInterface(this);
 
 	ui->setupUi(this);
+	ui->profileMenu->menuAction()->setVisible(false);
 	ui->previewDisabledWidget->setVisible(false);
 
 	/* Set up streaming connections */
