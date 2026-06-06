56df1f2b5fbb8ec9dc67a800749e7104caf6e647
Heiko Nickerl <mail@hnicke.de>
2026-06-06 17:22:01 +0200

    feat(ui): disable/hide more settings

--- frontend/settings/OBSBasicSettings.cpp
+++ frontend/settings/OBSBasicSettings.cpp
@@ -1,17 +1,17 @@
 /******************************************************************************
  Copyright (C) 2023 by Lain Bailey <lain@obsproject.com>
  Philippe Groarke <philippe.groarke@gmail.com>
- 
+
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 2 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -325,6 +325,8 @@ OBSBasicSettings::OBSBasicSettings(QWidget *parent)
 
 	ui->setupUi(this);
 	ui->listWidget->item(Pages::STREAM)->setHidden(true);
+	ui->groupBox_13->setHidden(true); // System Tray
+	ui->advNetworkGroupBox->setHidden(true);
 
 	main->EnableOutputs(false);
 
@@ -5792,6 +5794,7 @@ void OBSBasicSettings::UpdateMultitrackVideo()
 void OBSBasicSettings::ApplyManagedOutputLocks()
 {
 	QWidget *lockedWidgets[] = {
+		ui->audioGeneralGroupBox,
 		ui->outputMode,
 		ui->advOutAEncoder,
 		ui->advOutEncoder,
